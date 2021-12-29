1d44bda9f4967c77d6514b05208d5035a77442ce
semihcanburcak <59237301+semihcanburcak@users.noreply.github.com>
2021-12-29 23:16:45 +0100

    Online

---
+++ unityTest/scb/MateRun3_2/MAZE RUN 3/Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //Connect to Server
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(); //This will also allow us to join rooms later on, but for now we are simply joining a lobby
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+
+
+}
---
+++ unityTest/scb/MateRun3_2/MAZE RUN 3/Assets/Scripts/CreateAndJoinRooms.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public InputField createInput;
+    public InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        SceneManager.LoadScene("SchoolSceneDayMazeRun");
+    }
+
+}
